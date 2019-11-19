6a43d22e0d0813196c80809e1712f3e96c14c86f
Rishi <rishi.kavikondala@gmail.com>
2019-11-18 20:38:56 -0800

    Style changes

--- scripts/graphone.R
+++ scripts/graphone.R
@@ -5,6 +5,8 @@ library("dplyr")
 library("tidyr")
 library("ggplot2")
 library("plotly")
+library("styler")
+library("lintr")
 
 # Load data
 crime_data <- read.csv("./data/dataset_one.csv")
@@ -16,7 +18,8 @@ precinct_totals <- crime_data %>%
 
 # Make a bar chart using ggplot2
 graph_one <- ggplot(
-  data = precinct_totals, aes(x = Precinct, y = n, fill = Precinct)) +
-  geom_bar(stat = "identity") + 
-  coord_polar() + 
+  data = precinct_totals, aes(x = Precinct, y = n, fill = Precinct)
+) +
+  geom_bar(stat = "identity") +
+  coord_polar() +
   ggtitle("Crimes Per Precinct from 2008 to Present Day")
