b6ecc0c3bf26786ed12836709636194341c43859
Rishi <rishi.kavikondala@gmail.com>
2019-11-18 21:04:34 -0800

    Added graph one description

--- scripts/graphthree.R
+++ scripts/graphthree.R
@@ -1,26 +1,27 @@
 library(plotly)
 library(dplyr)
 
-#number of data
+# number of data
 data <- read.csv("./data/crime_data.csv")
 
-#total number of crime reported
+# total number of crime reported
 num_of_crime_reported <- nrow(data)
-DT <- data %>% select(Report.Number, Neighborhood) %>%
-      group_by(Neighborhood) %>%
-      summarise(sum = n())
+d_t <- data %>%
+  select(Report.Number, Neighborhood) %>%
+  group_by(Neighborhood) %>%
+  summarise(sum = n())
 
+# filter and count the number of crimes in the Neighborhood data
 
-#filter and count the number of crimes in the Neighborhood data
-
-#create pie chart
-bar_chart <- plot_ly(DT, 
-                     x = ~Neighborhood, 
-                     y = ~sum, 
-                     type = 'bar',
-                     marker = list(color = 'rgb(229,204,255)')) %>%
-  layout(title = "Number of crimes Reported in Seattle Neighborhood",
-         xaxis = list(title = ""),
-         yaxis = list(title = ""))
-
-
+# create pie chart
+bar_chart <- plot_ly(d_t,
+  x = ~Neighborhood,
+  y = ~sum,
+  type = "bar",
+  marker = list(color = "rgb(229,204,255)")
+) %>%
+  layout(
+    title = "Number of crimes Reported in Seattle Neighborhood",
+    xaxis = list(title = ""),
+    yaxis = list(title = "")
+  )
