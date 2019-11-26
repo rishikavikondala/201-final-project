e3633768de602339fe2d77b65bde651211ba0fa1
Solomon Kim <solorice123@gmail.com>
2019-11-26 12:44:24 -0800

    first page setup

--- seattle-crime/ui.R
+++ seattle-crime/ui.R
@@ -9,25 +9,17 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
+first_page <- tabPanel(
+    "Introduction Page",
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+            
         )
     )
-))
+    
+)
+
+ui <- navbarPage(
+    "final deliverable",
+    first_page
+)
